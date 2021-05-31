649648a9c4c2a4a66c83232d88c61373891535a3
zhuang ma <zhuangzhimhao@yahoo.com>
2021-05-31 16:10:26 +0800

    new file:   ASCA.sh 	new file:   IDSYS.sh 	new file:   ascaapp.sh 	new file:   base_inspection.sh 	modified:   hosts_list.conf 	new file:   idsysapp.sh 	renamed:    xj.ksh -> main_inspection.sh 	new file:   sz_hosts_list.conf

---
+++ base_inspection.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#This is a base inspection script.
+
+#Check filesystem partiton.
+cat /proc/mounts | awk -v current_host=$(uname -n) '
+BEGIN{
+    fs_health = 1
+}
+$4 ~ /^ro/{
+    printf "%s WARNING: %s filesystem read only.\n", current_host, $2
+    fs_health = 0
+}
+END{
+    if (fs_health == 1)
+    {
+        printf "%s filesystem: OK.\n", current_host
+    }
+}'
+
+#Check disk space.
+df -h | sed '1d; s/%//' | awk -v current_host=$(uname -n) '
+BEGIN{
+    disk_health = 1
+}
+{
+    if ($5 >= 90)
+    {
+        printf "%s WARNING: %s used over %d%.\n", current_host, $1, $5
+        disk_health = 0
+    }
+}
+END{
+    if (disk_health == 1)
+    {
+        printf "%s disk_space: OK.\n", current_host
+    }
+}'
+
+#Check memery space.
+free -m | awk -v current_host=$(uname -m) '
+BEGIN{
+    memery_health = 1
+}
+NR == 2{
+    memery_total = $2
+}
+NR == 3{
+    memery_used = $3 / memery_total * 100
+    if (memery_used >= 90)
+    {
+        printf "%s WARNING: memery used over %.2f%%.\n", current_host, memery_used
+        memery_health = 0
+    }
+}
+END{
+    if (memery_health == 1)
+    {
+        printf "%s memery_space: OK.\n", current_host
+    }
+}'
+
+#Check network.
+#ping
+
+#Check login record.
+#reboot   system boot  2.6.18-128.el5   Wed May 12 07:57         (7+02:25)
+last -s $(date -d "1 day ago" "+%Y%m%d" )| awk -v current_host=$(uname -m) '
+BEGIN{
+    login_health = 1
+}
+/reboot/{
+    printf "%s WARNING: rebooted since 24 hours.\n", current_host
+    login_health = 0
+}
+END{
+    if (login_health == 1)
+    {
+        printf "%s login_record: OK.\n", current_host
+    }
+}
--- main_inspection.sh
+++ main_inspection.sh
@@ -1,86 +1,5 @@
 #!/bin/ksh
 
-function base_check
-{
-    #Check filesystem partiton.
-    cat /proc/mounts | awk -v current_host=$(uname -n) '
-    BEGIN{
-        fs_health = 1
-    }
-    $4 ~ /^ro/{
-        printf "%s WARNING: %s filesystem read only.\n", current_host, $2 
-        fs_health = 0
-    }
-    END{
-        if (fs_health == 1)
-        {
-            printf "%s filesystem: OK.\n", current_host
-        }
-    }'
-
-    #Check disk space.
-    df -h | sed '1d; s/%//' | awk -v current_host=$(uname -n) '
-    BEGIN{
-        disk_health = 1
-    }
-    {
-        if ($5 >= 90)
-        {
-            printf "%s WARNING: %s used over %d%.\n", current_host, $1, $5
-            disk_health = 0
-        }
-    }
-    END{
-        if (disk_health == 1)
-        {
-            printf "%s disk_space: OK.\n", current_host
-        }
-    }'
-
-    #Check memery space.
-    free -m | awk -v current_host=$(uname -m) '
-    BEGIN{
-        memery_health = 1
-    }
-    NR == 2{
-        memery_total = $2
-    }
-    NR == 3{
-        memery_used = $3 / memery_total * 100 
-        if (memery_used >= 90)
-        {
-            printf "%s WARNING: memery used over %.2f%%.\n", current_host, memery_used
-            memery_health = 0
-        }
-    }
-    END{
-        if (memery_health == 1)
-        {
-            printf "%s memery_space: OK.\n", current_host
-        }
-    }'
-
-    #Check network.
-    #ping 
-
-    #Check login record.
-    #reboot   system boot  2.6.18-128.el5   Wed May 12 07:57         (7+02:25)
-    last -s $(date -d "1 day ago" "+%Y%m%d" )| awk -v current_host=$(uname -m) '
-    BEGIN{
-        login_health = 1
-    }
-    /reboot/{
-        printf "%s WARNING: rebooted since 24 hours.\n", current_host
-        login_health = 0
-    }
-    END{
-        if (login_health == 1)
-        {
-            printf "%s login_record: OK.\n", current_host
-        }
-    }'
-}
-
 function database_status
 {
 }
@@ -90,10 +9,10 @@ function ascaapp_check
     printf "############################################################################################\n"
     printf "#                                     ascaapp                                              #\n"
     printf "############################################################################################\n"
-    
+
     if test $(ps -ef | grep 'java' | grep -v 'grep' | wc -l) -eq 2
     then
-        printf "ascaapp precess: OK.\n" 
+        printf "ascaapp precess: OK.\n"
     else
         printf "ascaapp WARNING: no java process.\n"
     fi
@@ -116,7 +35,7 @@ function idsysapp_check
 
     if test $(ps -ef | grep 'java' | grep -v 'grep' | wc -l) -eq 2
     then
-        printf "idsysapp precess: OK.\n" 
+        printf "idsysapp precess: OK.\n"
     else
         printf "idsysapp WARNING: no java process.\n"
     fi
@@ -132,7 +51,7 @@ function IDSYS_check
 }
 
 awk '' hosts_list.conf
-for
+
 if test ${#} gt 0
 then
     while getops n:h:v current_opt
@@ -149,4 +68,8 @@ then
         esac
     done
 else
+    for i in ${!hosts_list[*]}
+        ssh -T ${hosts_list[${i}]} base_check
+        ssh $u@$addr 'cat | bash /dev/stdin' "$@" < "$scriptfile"
+        hosts_list(i)
 fi
