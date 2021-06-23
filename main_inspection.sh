d007197d82d60d188d714a555e24797b684ca560
Ma Zhuang <mz@MzHost.my.domain>
2021-06-24 00:45:33 +0800

    modified:   main_inspection.sh

--- main_inspection.sh
+++ main_inspection.sh
@@ -2,34 +2,46 @@
 
 declare -A hosts_list
 
+function execute_script
+{
+    for i in ${!hosts_list[@]}
+    do
+        #Execute base inspection script.
+        if test -e base_inspection.sh
+        then
+            ssh -T ${hosts_list[${i}]} < base_inspection.sh
+        fi
+
+        #Execute spectify inspection script.
+        if test -e ${i}".sh"
+        then
+            ssh -T ${hosts_list[${i}]} < ${i}".sh"
+        fi
+    done
+}
+
 eval $(awk 'NR > 1{printf "hosts_list[%s]=%s\n", $1, $2}' hosts_list.conf)
 
+#Main
 if test ${#} gt 0
 then
     while getops n:h:v current_opt
     do
         case ${current_opt} in
             n)
+                execute_script ${OPTARG} host_name
             ;;
             h)
+                execute_script ${OPTARG} ip
             ;;
             v)
+                #awk error.log
             ;;
             *)
+                #print error
             ;;
         esac
     done
 else
-    for i in ${!hosts_list[@]}
-    do
-        #Execute base inspection script.
-        if test -e base_inspection.sh
-            ssh -T ${hosts_list[${i}]} < base_inspection.sh
-        fi
-
-        #Execute spectify inspection script.
-        if test -e ${i}".sh"
-            ssh -T ${hosts_list[${i}]} < ${i}"i.sh"
-        fi
-    done
+    execute_script
 fi
