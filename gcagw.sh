6b20bf0c1824dca2cf099b42991da5b52d78140f
Mz Zhuang <zhuangzhimhao@yahoo.com>
2021-08-07 06:38:28 +0800

    modified:   gcagw.sh

--- gcagw.sh
+++ gcagw.sh
@@ -8,30 +8,43 @@ string_today=$(date "+%Y%m%d%H%M")
 root_capacity=$(df -h | sed -n '/\/$/s/%//p' | awk '{print $5}')
 #获取/gci_home分區容量.
 gci_home_capacity=$(df -h | sed -n '/gcagw/s/%//p' | awk '{print $5}')
+#如未获取得值,設為默認值.
+printf "root_capacity: %d.\n" ${root_capacity:=100}
+printf "gci_home_capacity: %d.\n" ${gci_home_capacity:=0}
 
 #判断磁盤容量已满.
-if test root_capacity -lt 85 -a gci_home_capacity -gt 90
+if test $(printf "${root_capacity} < 85\n" | bc) -eq 1 -a $(printf "${gci_home_capacity} > 90\n" | bc) -eq 1
 then
     #轉移系統自動打包的tar.gz文件.
-    for i in ${source_dir}*log.tar.gz
-    do
-        printf "mv:\n"
-        mv -v ${i} ${destinatione_dir}
-        #删除不需要的log文件.
-        if test ${?} -eq 0
+    tar_old=$(ls ${source_dir} | grep 'log\.tar\.gz$')
+    if test "${tar_old}" != ""
+    then
+        for i in ${tar_old}
+        do
+            mv -v ${source_dir}${i} ${destinatione_dir}
+            #删除不需要的log文件.
+            if test ${?} -eq 0
+            then
+                rm -v ${source_dir}${i:0:8}*log
+            fi
+        done
+    fi
+
+    #打包當天日誌文件,深圳无需執行.
+    province_code=$(uname -n)
+    if test "${province_code::2}" = "44"
+    then
+        file_today=$(ls ${source_dir} | grep -E "${string_today:0:8}[[:digit:]]{2}.log")
+        if test "${file_today}" != ""
         then
-            del_date=$(basename ${i})
-            rm -v ${source_dir}${del_date:8}*log
+            for j in $(tar -czv -f ${destinatione_dir}${string_today}.log.tar.gz -C ${source_dir} ${file_today})
+            do
+                #删除當天不需要的log文件.
+                if test ${?} -eq 0
+                then
+                    rm -v ${source_dir}${j}
+                fi
+            done
         fi
-    done
-    #打包當天日誌文件.
-    printf "rm:\n"
-    for j in $(tar -czv -f ${destinatione_dir}${string_today}.log.tar.gz -C ${source_dir} ${string_today:8}??.log)
-    do
-        #删除當天不需要的log文件.
-        if test ${?} -eq 0
-        then
-            rm -v ${source_dir}${j}
-        fi
-    done
+    fi
 fi
