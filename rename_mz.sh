c8e8ca1caba19a76e54b005807855bcf586b149e
Ma Zhuang <zhuangzhimhao@outlook.com>
2024-07-18 07:20:47 +0800

    modified:   rename_mz.sh

--- rename_mz.sh
+++ rename_mz.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# eg: reanme_mz.sh "string1" "string2" files
+#
+if test ${#} -ne 4
+then
+    printf "Invalid parameter!\n"
+    exit 1
+fi
+
+for i in $(ls ${3})
+do
+    mv -v ${i} ${i/${1}/${2}}
+done
