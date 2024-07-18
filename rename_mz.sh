965cc50b39bdedc544b112bc6ab821bfc9cf4ee0
Ma Zhuang <zhuangzhimhao@outlook.com>
2024-07-18 17:59:36 +0800

    modified:   rename_mz.sh

--- rename_mz.sh
+++ rename_mz.sh
@@ -1,14 +1,56 @@
 #!/bin/bash
 
-# eg: reanme_mz.sh "string1" "string2" files
+# eg: reanme_mz.sh -i "string1" -o "string2" -f files
 #
-if test ${#} -ne 4
+set -e
+
+parameter_input=""
+parameter_output=""
+i=0
+j=1
+files_before=()
+files_after=()
+
+while getopts "i:o:f:" current_opt
+do
+    case ${current_opt} in
+        i)
+            parameter_input=${OPTARG}
+            ;;
+        o)
+            parameter_output=${OPTARG}
+            ;;
+        *)
+            echo "Wrong parameter!"
+            exit 1
+            ;;
+    esac
+done
+if test -z ${parameter_input}
 then
-    printf "Invalid parameter!\n"
+    echo "Wrong parameter!"
     exit 1
 fi
-
-for i in $(ls ${3})
+# List changes.
+shift $(echo "${OPTIND} - 1" | bc)
+amount_of_files=${#}
+while test ${i} -lt ${amount_of_files}
 do
-    mv -v ${i} ${i/${1}/${2}}
+    files_before[${i}]=${1}
+    files_after[${i}]=${files_before[${i}]/${parameter_input}/${parameter_output}}
+    echo "${files_before[${i}]} --> ${files_after[${i}]}"
+    i=$(echo "${i} + 1" | bc)
+    j=$(echo "${j} + 1" | bc)
+    shift
 done
+# Apply changes.
+read -p "Apply above changes?(Y/n): " answer
+i=0
+if test ${answer} = "Y"
+then
+    while test ${i} -lt ${#files_before[@]}
+    do
+        mv -vi ${files_before[${i}]} ${files_after[${i}]}
+        i=$(echo "${i} + 1" | bc)
+    done
+fi
